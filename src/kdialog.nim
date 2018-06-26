2d13610a6f26b02f50ad2fe85d214a42153a5433
juancarlospaco <juancarlospaco@gmail.com>
2018-06-26 17:52:53 -0300

    Lower case const

--- src/kdialog.nim
+++ src/kdialog.nim
@@ -30,112 +30,112 @@ import os, osproc, strformat
 
 
 const
-  VERSION* = staticExec("kdialog --version") ## KDialog Version that this Nim module was using when compiled.
-  AUTHORS* = staticExec("kdialog --author")  ## KDialog Authors.
-var DEFAULT_TITLE* = "Nim KDialog"           ## Default Title string for the GUI windows.
+  version* = staticExec("kdialog --version") ## KDialog version that this Nim module was using when compiled.
+  authors* = staticExec("kdialog --author")  ## KDialog authors.
+var default_title* = "Nim KDialog"           ## Default Title string for the GUI windows.
 
 
-proc yesno*(text_or_html: string, title: string= DEFAULT_TITLE): tuple =
+proc yesno*(text_or_html: string, title: string= default_title): tuple =
   execCmdEx(fmt"kdialog --title '{title}' --yesno '{text_or_html}'")
 
-proc yesnocancel*(text_or_html: string, title: string= DEFAULT_TITLE): tuple =
+proc yesnocancel*(text_or_html: string, title: string= default_title): tuple =
   execCmdEx(fmt"kdialog --title '{title}' --yesnocancel '{text_or_html}'")
 
-proc warningyesno*(text_or_html: string, title: string= DEFAULT_TITLE): tuple =
+proc warningyesno*(text_or_html: string, title: string= default_title): tuple =
   execCmdEx(fmt"kdialog --title '{title}' --warningyesno '{text_or_html}'")
 
-proc warningcontinuecancel*(text_or_html: string, title: string= DEFAULT_TITLE): tuple =
+proc warningcontinuecancel*(text_or_html: string, title: string= default_title): tuple =
   execCmdEx(fmt"kdialog --title '{title}' --warningcontinuecancel '{text_or_html}'")
 
-proc warningyesnocancel*(text_or_html: string, title: string= DEFAULT_TITLE): tuple =
+proc warningyesnocancel*(text_or_html: string, title: string= default_title): tuple =
   execCmdEx(fmt"kdialog --title '{title}' --warningyesnocancel '{text_or_html}'")
 
-proc sorry*(text_or_html: string, title: string= DEFAULT_TITLE): tuple =
+proc sorry*(text_or_html: string, title: string= default_title): tuple =
   execCmdEx(fmt"kdialog --title '{title}' --sorry '{text_or_html}'")
 
-proc detailedsorry*(text_or_html: string, details: string, title: string= DEFAULT_TITLE): tuple =
+proc detailedsorry*(text_or_html: string, details: string, title: string= default_title): tuple =
   execCmdEx(fmt"kdialog --title '{title}' --detailedsorry '{text_or_html}' '{details}'")
 
-proc error*(text_or_html: string, title: string= DEFAULT_TITLE): tuple =
+proc error*(text_or_html: string, title: string= default_title): tuple =
   execCmdEx(fmt"kdialog --title '{title}' --error '{text_or_html}'")
 
-proc detailederror*(text_or_html: string, details: string, title: string= DEFAULT_TITLE): tuple =
+proc detailederror*(text_or_html: string, details: string, title: string= default_title): tuple =
   execCmdEx(fmt"kdialog --title '{title}' --detailederror '{text_or_html}' '{details}'")
 
-proc msgbox*(text_or_html: string, title: string= DEFAULT_TITLE): tuple =
+proc msgbox*(text_or_html: string, title: string= default_title): tuple =
   execCmdEx(fmt"kdialog --title '{title}' --msgbox '{text_or_html}'")
 
-proc inputbox*(text_or_html: string, init: string, title: string= DEFAULT_TITLE): tuple =
+proc inputbox*(text_or_html: string, init: string, title: string= default_title): tuple =
   execCmdEx(fmt"kdialog --title '{title}' --inputbox '{text_or_html}' '{init}'")
 
-proc password*(text_or_html: string, init: string, title: string= DEFAULT_TITLE): tuple =
+proc password*(text_or_html: string, init: string, title: string= default_title): tuple =
   execCmdEx(fmt"kdialog --title '{title}' --password '{text_or_html}'")
 
-proc newpassword*(text_or_html: string, title: string= DEFAULT_TITLE): tuple =
+proc newpassword*(text_or_html: string, title: string= default_title): tuple =
   execCmdEx(fmt"kdialog --title '{title}' --newpassword '{text_or_html}'")
 
-proc textbox*(text_or_html: string, title: string= DEFAULT_TITLE): tuple =
+proc textbox*(text_or_html: string, title: string= default_title): tuple =
   execCmdEx(fmt"kdialog --title '{title}' --textbox '{text_or_html}'")
 
-proc textinputbox*(text_or_html: string, init: string, title: string= DEFAULT_TITLE): tuple =
+proc textinputbox*(text_or_html: string, init: string, title: string= default_title): tuple =
   execCmdEx(fmt"kdialog --title '{title}' --textinputbox '{text_or_html}' '{init}'")
 
-proc passivepopup*(text_or_html: string, timeout: int8, title: string= DEFAULT_TITLE): tuple =
+proc passivepopup*(text_or_html: string, timeout: int8, title: string= default_title): tuple =
   execCmdEx(fmt"kdialog --title '{title}' --passivepopup '{text_or_html}' {timeout}")
 
-proc getopenfilename*(startDir: string= getCurrentDir(), filter: string= "*.*", multiple: bool=false, title: string= DEFAULT_TITLE): tuple =
+proc getopenfilename*(startDir: string= getCurrentDir(), filter: string= "*.*", multiple: bool=false, title: string= default_title): tuple =
   let multi = if multiple: "--multiple" else: ""
   execCmdEx(fmt"kdialog --title '{title}' --getopenfilename '{startDir}' '{filter}' {multi}")
 
-proc getsavefilename*(startDir: string= getCurrentDir(), filter: string= "*.*", title: string= DEFAULT_TITLE): tuple =
+proc getsavefilename*(startDir: string= getCurrentDir(), filter: string= "*.*", title: string= default_title): tuple =
   execCmdEx(fmt"kdialog --title '{title}' --getsavefilename '{startDir}' '{filter}'")
 
-proc getexistingdirectory*(startDir: string= getCurrentDir(), title: string= DEFAULT_TITLE): tuple =
+proc getexistingdirectory*(startDir: string= getCurrentDir(), title: string= default_title): tuple =
   execCmdEx(fmt"kdialog --title '{title}' --getexistingdirectory '{startDir}'")
 
-proc getopenurl*(startDir: string= getCurrentDir(), filter: string= "*.*", multiple: bool= false, title: string= DEFAULT_TITLE): tuple =
+proc getopenurl*(startDir: string= getCurrentDir(), filter: string= "*.*", multiple: bool= false, title: string= default_title): tuple =
   let multi = if multiple: "--multiple" else: ""
   execCmdEx(fmt"kdialog --title '{title}' --getopenurl '{startDir}' '{filter}' {multi}")
 
-proc getsaveurl*(startDir: string= getCurrentDir(), filter: string= "*.*", title: string= DEFAULT_TITLE): tuple =
+proc getsaveurl*(startDir: string= getCurrentDir(), filter: string= "*.*", title: string= default_title): tuple =
   execCmdEx(fmt"kdialog --title '{title}' --getsaveurl '{startDir}' '{filter}'")
 
-proc geticon*(title: string= DEFAULT_TITLE): tuple =
+proc geticon*(title: string= default_title): tuple =
   execCmdEx(fmt"kdialog --title '{title}' --geticon")
 
-proc getcolor*(title: string= DEFAULT_TITLE): tuple =
+proc getcolor*(title: string= default_title): tuple =
   execCmdEx(fmt"kdialog --title '{title}' --getcolor")
 
-proc slider*(text_or_html: string, minimum: int, maximum: int, steps: int8, title: string= DEFAULT_TITLE): tuple =
+proc slider*(text_or_html: string, minimum: int, maximum: int, steps: int8, title: string= default_title): tuple =
   execCmdEx(fmt"kdialog --title '{title}' --slider '{text_or_html}' {minimum} {maximum} {steps}")
 
-proc calendar*(text_or_html: string, title: string= DEFAULT_TITLE): tuple =
+proc calendar*(text_or_html: string, title: string= default_title): tuple =
   execCmdEx(fmt"kdialog --title '{title}' --calendar '{text_or_html}'")
 
 
 ################################################################################
 # TODO: These ones needs more work to allow flexible parameters, they are WIP.
 #
-# proc checklist*(text_or_html: string, separate_output: bool=false, title: string= DEFAULT_TITLE): tuple =
+# proc checklist*(text_or_html: string, separate_output: bool=false, title: string= default_title): tuple =
 #   let sepa = if separate_output: "--separate-output" else: ""
 #   execCmdEx(fmt"kdialog --title '{title}' --checklist '{text_or_html}' {sepa}")
 #
-# proc combobox*(text_or_html: string, title: string= DEFAULT_TITLE): tuple =
+# proc combobox*(text_or_html: string, title: string= default_title): tuple =
 #   execCmdEx(fmt"kdialog --title '{title}' --combobox '{text_or_html}'")
 #
-# proc menu*(text_or_html: string, title: string= DEFAULT_TITLE): tuple =
+# proc menu*(text_or_html: string, title: string= default_title): tuple =
 #   execCmdEx(fmt"kdialog --title '{title}' --menu '{text_or_html}'")
 #
-# proc progressbar*(text_or_html: string, value: int8, title: string= DEFAULT_TITLE): tuple =
+# proc progressbar*(text_or_html: string, value: int8, title: string= default_title): tuple =
 #   execCmdEx(fmt"kdialog --title '{title}' --progressbar '{text_or_html}' {value}")
 #
-# proc radiolist*(text_or_html: string, title: string= DEFAULT_TITLE): tuple =
+# proc radiolist*(text_or_html: string, title: string= default_title): tuple =
 #   execCmdEx(fmt"kdialog --title '{title}' --radiolist '{text_or_html}'")
 
 
 when not defined(release):
   if is_main_module:
-    echo VERSION, AUTHORS, DEFAULT_TITLE
+    echo version, authors, default_title
     echo yesno "Love Nim?"
     echo yesnocancel "Is Nim Awesome?"
     echo warningyesno "Update Nimble index!"
